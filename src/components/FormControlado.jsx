70093b20cfa60b93f076b8f2e3cedb8376dd2298
Giovanny Vargas <yovaz05@gmail.com>
2023-09-20 18:30:52 -0500

    clase 20-09-2023

---
+++ src/components/FormControlado.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const FormControlado = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handlerInputEmail = (e) => {
+    console.log(e);
+  };
+  const handlerInputPassword = (e) => {
+    console.log(e);
+  };
+
+  return (
+    <>
+      <input
+        type="text"
+        name="email"
+        placeholder="Ingrese Email"
+        autoComplete="off"
+        value={form.email}
+        onChange={handlerInputEmail}
+      />
+
+      <input
+        type="password"
+        name="password"
+        placeholder="Ingrese password"
+        autoComplete="off"
+        value={form.password}
+        onChange={handlerInputPassword}
+      />
+    </>
+  );
+};
+
+export default FormControlado;
